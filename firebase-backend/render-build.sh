79cb43dc43e0f0878f108963f44eaac24688d1a8
UzayC <uzaycolak@gmail.com>
2025-01-03 14:34:36 +0300

    Fix JAVA_HOME environment variable

--- firebase-backend/render-build.sh
+++ firebase-backend/render-build.sh
@@ -3,5 +3,9 @@
 # Maven'i çalıştırılabilir yap
 chmod +x mvnw
 
+# JAVA_HOME'u manuel olarak ayarla
+export JAVA_HOME=/opt/java/openjdk
+export PATH=$JAVA_HOME/bin:$PATH
+
 # Projeyi Maven ile build et
 ./mvnw clean install

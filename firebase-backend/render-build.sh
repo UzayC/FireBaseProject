8eb63bd54bacbbd7ae1f30bd026bd38f71ec36de
UzayC <uzaycolak@gmail.com>
2025-01-03 12:23:40 +0300

    JAVA_HOME and OpenJDK installation

--- firebase-backend/render-build.sh
+++ firebase-backend/render-build.sh
@@ -5,5 +5,11 @@ curl -o apache-maven.tar.gz https://downloads.apache.org/maven/maven-3/3.8.8/bin
 tar -xzf apache-maven.tar.gz
 export PATH=$PATH:$(pwd)/apache-maven-3.8.8/bin
 
+# Java'yı indir ve kur
+curl -o openjdk.tar.gz https://download.java.net/java/GA/jdk17/0d1cfde4252546c693194c345fcedc12/35/GPL/openjdk-17_linux-x64_bin.tar.gz
+tar -xzf openjdk.tar.gz
+export JAVA_HOME=$(pwd)/jdk-17
+export PATH=$PATH:$JAVA_HOME/bin
+
 # Maven ile projeyi build et
 mvn clean install
